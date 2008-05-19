d33f6f88a5dd159bf3387eeec004993cbb5d88eb
Paolo Bonzini <bonzini@gnu.org>
2008-05-19 16:39:40 +0200

    more APIs, add list of glGet parameters from PerlGL

--- packages/opengl/gstGl.c
+++ packages/opengl/gstGl.c
@@ -277,6 +277,43 @@ gst_opengl_glLoadTransposeMatrixv (OOP matrixOOP)
 }
 
 static int
+gst_opengl_glFogv_size (GLenum pname)
+{
+  switch (pname)
+    {
+    case GL_FOG_COLOR:
+      return 4;
+
+    case GL_FOG_MODE:
+    case GL_FOG_DENSITY:
+    case GL_FOG_START:
+    case GL_FOG_END:
+    case GL_FOG_INDEX:
+      return 1;
+
+    default:
+      return 0;
+    }
+}
+
+static int
+gst_opengl_glFogv (GLenum pname, OOP paramsOOP)
+{
+  GLfloat *params;
+  int size = gst_opengl_glFogv_size (pname);
+  if (size == 0)
+    return GL_INVALID_ENUM;
+
+  params = (GLfloat *) alloca (sizeof (GLfloat) * size);
+  params = gst_opengl_oop_to_array (params, paramsOOP, size);
+  if (!params)
+    return GL_INVALID_VALUE;
+
+  glFogfv (pname, params);
+  return 0;
+}
+
+static int
 gst_opengl_glLightv_size (GLenum pname)
 {
   switch (pname)
@@ -319,6 +356,94 @@ gst_opengl_glLightv (GLenum face, GLenum pname, OOP paramsOOP)
   return 0;
 }
 
+
+static int
+gst_opengl_glTexParameterv_size (GLenum pname)
+{
+  switch (pname)
+    {
+#ifdef GL_EXT_texture_object
+    case GL_TEXTURE_PRIORITY_EXT:
+    case GL_TEXTURE_RESIDENT_EXT:
+      return 1;
+#endif
+
+#ifdef GL_VERSION_1_2
+    case GL_TEXTURE_WRAP_R:
+    case GL_TEXTURE_DEPTH:
+      return 1;
+
+    case GL_TEXTURE_MIN_LOD:
+    case GL_TEXTURE_MAX_LOD:
+    case GL_TEXTURE_BASE_LEVEL:
+    case GL_TEXTURE_MAX_LEVEL:
+      return 1;
+#endif
+
+    case GL_TEXTURE_MAG_FILTER:
+    case GL_TEXTURE_MIN_FILTER:
+    case GL_TEXTURE_WRAP_S:
+    case GL_TEXTURE_WRAP_T:
+    case GL_TEXTURE_PRIORITY:
+    case GL_TEXTURE_RESIDENT:
+      return 1;
+    case GL_TEXTURE_BORDER_COLOR:
+      return 4;
+    default:
+      return 0;
+    }
+}
+
+static int
+gst_opengl_glTexParameterv (GLenum face, GLenum pname, OOP paramsOOP)
+{
+  GLfloat *params;
+  int size = gst_opengl_glTexParameterv_size (pname);
+  if (size == 0)
+    return GL_INVALID_ENUM;
+
+  params = (GLfloat *) alloca (sizeof (GLfloat) * size);
+  params = gst_opengl_oop_to_array (params, paramsOOP, size);
+  if (!params)
+    return GL_INVALID_VALUE;
+
+  glTexParameterfv (face, pname, params);
+  return 0;
+}
+
+static int
+gst_opengl_glLightModelv_size (GLenum pname)
+{
+  switch (pname)
+    {
+    case GL_LIGHT_MODEL_AMBIENT:
+      return 4;
+
+    case GL_LIGHT_MODEL_LOCAL_VIEWER:
+    case GL_LIGHT_MODEL_TWO_SIDE:
+      return 1;
+
+    default:
+      return 0;
+    }
+}
+
+static int
+gst_opengl_glLightModelv (GLenum pname, OOP paramsOOP)
+{
+  GLfloat *params;
+  int size = gst_opengl_glLightModelv_size (pname);
+  if (size == 0)
+    return GL_INVALID_ENUM;
+
+  params = (GLfloat *) alloca (sizeof (GLfloat) * size);
+  params = gst_opengl_oop_to_array (params, paramsOOP, size);
+  if (!params)
+    return GL_INVALID_VALUE;
+
+  glLightModelfv (pname, params);
+  return 0;
+}
 
 static int
 gst_opengl_glTexEnvv_size (GLenum pname)
@@ -400,20 +525,331 @@ gst_opengl_glGetv_size (GLenum pname)
 {
   switch (pname)
     {
+#ifdef GL_EXT_polygon_offset
+    case GL_POLYGON_OFFSET_EXT:
+    case GL_POLYGON_OFFSET_FACTOR_EXT:
+    case GL_POLYGON_OFFSET_BIAS_EXT:
+      return 1;
+#endif
+
+#ifdef GL_VERSION_1_2
+    case GL_TEXTURE_3D:
+    case GL_UNPACK_SKIP_IMAGES:
+    case GL_UNPACK_IMAGE_HEIGHT:
+    case GL_PACK_SKIP_IMAGES:
+    case GL_PACK_IMAGE_HEIGHT:
+    case GL_MAX_3D_TEXTURE_SIZE:
+      return 1;
+
+    case GL_RESCALE_NORMAL:
+      return 1;
+
+    case GL_LIGHT_MODEL_COLOR_CONTROL:
+      return 1;
+
+    case GL_MAX_ELEMENTS_VERTICES:
+    case GL_MAX_ELEMENTS_INDICES:
+      return 1;
+#endif
+
+#ifdef GL_VERSION_1_1
+    case GL_TEXTURE_COORD_ARRAY:
+    case GL_TEXTURE_COORD_ARRAY_SIZE:
+    case GL_TEXTURE_COORD_ARRAY_STRIDE:
+    case GL_TEXTURE_COORD_ARRAY_TYPE:
+    case GL_VERTEX_ARRAY:
+    case GL_VERTEX_ARRAY_SIZE:
+    case GL_VERTEX_ARRAY_STRIDE:
+    case GL_VERTEX_ARRAY_TYPE:
+    case GL_POLYGON_OFFSET_FACTOR:
+    case GL_POLYGON_OFFSET_UNITS:
+    case GL_POLYGON_OFFSET_FILL:
+    case GL_POLYGON_OFFSET_LINE:
+    case GL_POLYGON_OFFSET_POINT:
+    case GL_NORMAL_ARRAY:
+    case GL_NORMAL_ARRAY_STRIDE:
+    case GL_NORMAL_ARRAY_TYPE:
+    case GL_INDEX_LOGIC_OP:
+    case GL_INDEX_ARRAY:
+    case GL_INDEX_ARRAY_STRIDE:
+    case GL_INDEX_ARRAY_TYPE:
+    case GL_EDGE_FLAG_ARRAY:
+    case GL_EDGE_FLAG_ARRAY_STRIDE:
+    case GL_COLOR_ARRAY:
+    case GL_COLOR_ARRAY_SIZE:
+    case GL_COLOR_ARRAY_STRIDE:
+    case GL_COLOR_ARRAY_TYPE:
+    case GL_COLOR_LOGIC_OP:
+      return 1;
+#endif
+
+#ifdef GL_EXT_cmyka
+    case GL_PACK_CMYK_HINT_EXT:
+    case GL_UNPACK_CMYK_HINT_EXT:
+      return 1;
+#endif
+
+#ifdef GL_EXT_vertex_array
+    case GL_VERTEX_ARRAY_EXT:
+    case GL_VERTEX_ARRAY_SIZE_EXT:
+    case GL_VERTEX_ARRAY_STRIDE_EXT:
+    case GL_VERTEX_ARRAY_TYPE_EXT:
+    case GL_VERTEX_ARRAY_COUNT_EXT:
+    case GL_NORMAL_ARRAY_EXT:
+    case GL_NORMAL_ARRAY_STRIDE_EXT:
+    case GL_NORMAL_ARRAY_TYPE_EXT:
+    case GL_NORMAL_ARRAY_COUNT_EXT:
+    case GL_COLOR_ARRAY_EXT:
+    case GL_COLOR_ARRAY_SIZE_EXT:
+    case GL_COLOR_ARRAY_STRIDE_EXT:
+    case GL_COLOR_ARRAY_TYPE_EXT:
+    case GL_COLOR_ARRAY_COUNT_EXT:
+    case GL_INDEX_ARRAY_EXT:
+    case GL_INDEX_ARRAY_STRIDE_EXT:
+    case GL_INDEX_ARRAY_TYPE_EXT:
+    case GL_INDEX_ARRAY_COUNT_EXT:
+    case GL_TEXTURE_COORD_ARRAY_EXT:
+    case GL_TEXTURE_COORD_ARRAY_SIZE_EXT:
+    case GL_TEXTURE_COORD_ARRAY_STRIDE_EXT:
+    case GL_TEXTURE_COORD_ARRAY_TYPE_EXT:
+    case GL_TEXTURE_COORD_ARRAY_COUNT_EXT:
+    case GL_EDGE_FLAG_ARRAY_EXT:
+    case GL_EDGE_FLAG_ARRAY_STRIDE_EXT:
+    case GL_EDGE_FLAG_ARRAY_COUNT_EXT:
+      return 1;
+#endif
+
+#if defined GL_EXT_clip_volume_hint && defined GL_VOLUME_CLIPPING_HINT_EXT
+    case GL_VOLUME_CLIPPING_HINT_EXT:
+      return 1;
+#endif
+
+    case GL_ACCUM_ALPHA_BITS:
+    case GL_ACCUM_RED_BITS:
+    case GL_ACCUM_BLUE_BITS:
+    case GL_ACCUM_GREEN_BITS:
+    case GL_ALPHA_BIAS:
+    case GL_ALPHA_BITS:
+    case GL_ALPHA_SCALE:
+    case GL_ALPHA_TEST:
+    case GL_ALPHA_TEST_FUNC:
+    case GL_ALPHA_TEST_REF:
+    case GL_ATTRIB_STACK_DEPTH:
+    case GL_AUTO_NORMAL:
+    case GL_AUX_BUFFERS:
+    case GL_BLEND:
+    case GL_BLEND_DST:
+    case GL_BLEND_EQUATION_EXT:
+    case GL_BLEND_SRC:
+    case GL_BLUE_BIAS:
+    case GL_BLUE_BITS:
+    case GL_BLUE_SCALE:
+    case GL_CLIENT_ATTRIB_STACK_DEPTH:
+    case GL_COLOR_MATERIAL:
+    case GL_COLOR_MATERIAL_FACE:
+    case GL_COLOR_MATERIAL_PARAMETER:
+    case GL_CULL_FACE:
+    case GL_CULL_FACE_MODE:
+    case GL_CURRENT_INDEX:
+    case GL_CURRENT_RASTER_DISTANCE:
+    case GL_CURRENT_RASTER_INDEX:
+    case GL_CURRENT_RASTER_POSITION_VALID:
+    case GL_DEPTH_BIAS:
+    case GL_DEPTH_BITS:
+    case GL_DEPTH_CLEAR_VALUE:
+    case GL_DEPTH_FUNC:
+    case GL_DEPTH_SCALE:
+    case GL_DEPTH_TEST:
+    case GL_DEPTH_WRITEMASK:
+    case GL_DITHER:
+    case GL_DOUBLEBUFFER:
+    case GL_DRAW_BUFFER:
+    case GL_EDGE_FLAG:
+    case GL_FOG_DENSITY:
+    case GL_FOG_END:
+    case GL_FOG_HINT:
+    case GL_FOG_INDEX:
+    case GL_FOG_MODE:
+    case GL_FOG_START:
+    case GL_FRONT_FACE:
+    case GL_GREEN_BIAS:
+    case GL_GREEN_BITS:
+    case GL_GREEN_SCALE:
+    case GL_INDEX_BITS:
+    case GL_INDEX_CLEAR_VALUE:
+    case GL_INDEX_MODE:
+    case GL_INDEX_OFFSET:
+    case GL_INDEX_SHIFT:
+    case GL_INDEX_WRITEMASK:
+    case GL_LIGHTING:
+    case GL_LIGHT_MODEL_LOCAL_VIEWER:
+    case GL_LIGHT_MODEL_TWO_SIDE:
+    case GL_LINE_SMOOTH:
+    case GL_LINE_SMOOTH_HINT:
+    case GL_LINE_STIPPLE:
+    case GL_LINE_STIPPLE_PATTERN:
+    case GL_LINE_STIPPLE_REPEAT:
+    case GL_LINE_WIDTH:
+    case GL_LINE_WIDTH_GRANULARITY:
+    case GL_LIST_BASE:
+    case GL_LIST_INDEX:
+    case GL_LIST_MODE:
+    case GL_LOGIC_OP_MODE:
+    case GL_MAP1_COLOR_4:
+    case GL_MAP1_GRID_SEGMENTS:
+    case GL_MAP1_INDEX:
+    case GL_MAP1_NORMAL:
+    case GL_MAP1_TEXTURE_COORD_1:
+    case GL_MAP1_TEXTURE_COORD_2:
+    case GL_MAP1_TEXTURE_COORD_3:
+    case GL_MAP1_TEXTURE_COORD_4:
+    case GL_MAP1_VERTEX_3:
+    case GL_MAP1_VERTEX_4:
+    case GL_MAP2_INDEX:
+    case GL_MAP2_NORMAL:
+    case GL_MAP2_TEXTURE_COORD_1:
+    case GL_MAP2_TEXTURE_COORD_2:
+    case GL_MAP2_TEXTURE_COORD_3:
+    case GL_MAP2_TEXTURE_COORD_4:
+    case GL_MAP2_VERTEX_3:
+    case GL_MAP2_VERTEX_4:
+    case GL_MAP_COLOR:
+    case GL_MAP_STENCIL:
+    case GL_MATRIX_MODE:
+    case GL_MAX_CLIENT_ATTRIB_STACK_DEPTH:
+    case GL_MAX_ATTRIB_STACK_DEPTH:
+    case GL_MAX_CLIP_PLANES:
+    case GL_MAX_EVAL_ORDER:
+    case GL_MAX_LIGHTS:
+    case GL_MAX_LIST_NESTING:
+    case GL_MAX_MODELVIEW_STACK_DEPTH:
+    case GL_MAX_NAME_STACK_DEPTH:
+    case GL_MAX_PIXEL_MAP_TABLE:
+    case GL_MAX_PROJECTION_STACK_DEPTH:
+    case GL_MAX_TEXTURE_SIZE:
+    case GL_MAX_TEXTURE_STACK_DEPTH:
+    case GL_MODELVIEW_STACK_DEPTH:
+    case GL_NAME_STACK_DEPTH:
+    case GL_NORMALIZE:
+    case GL_PACK_ALIGNMENT:
+    case GL_PACK_LSB_FIRST:
+    case GL_PACK_ROW_LENGTH:
+    case GL_PACK_SKIP_PIXELS:
+    case GL_PACK_SKIP_ROWS:
+    case GL_PACK_SWAP_BYTES:
+    case GL_PERSPECTIVE_CORRECTION_HINT:
+    case GL_PIXEL_MAP_A_TO_A_SIZE:
+    case GL_PIXEL_MAP_B_TO_B_SIZE:
+    case GL_PIXEL_MAP_G_TO_G_SIZE:
+    case GL_PIXEL_MAP_I_TO_A_SIZE:
+    case GL_PIXEL_MAP_I_TO_B_SIZE:
+    case GL_PIXEL_MAP_I_TO_G_SIZE:
+    case GL_PIXEL_MAP_I_TO_I_SIZE:
+    case GL_PIXEL_MAP_I_TO_R_SIZE:
+    case GL_PIXEL_MAP_R_TO_R_SIZE:
+    case GL_PIXEL_MAP_S_TO_S_SIZE:
+    case GL_POINT_SIZE:
+    case GL_POINT_SIZE_GRANULARITY:
+    case GL_POINT_SIZE_RANGE:
+    case GL_POINT_SMOOTH:
+    case GL_POINT_SMOOTH_HINT:
+    case GL_POLYGON_SMOOTH:
+    case GL_POLYGON_SMOOTH_HINT:
+    case GL_POLYGON_STIPPLE:
+    case GL_PROJECTION_STACK_DEPTH:
+    case GL_READ_BUFFER:
+    case GL_RED_BIAS:
+    case GL_RED_BITS:
+    case GL_RED_SCALE:
+    case GL_RENDER_MODE:
+    case GL_RGBA_MODE:
+    case GL_SCISSOR_TEST:
+    case GL_SHADE_MODEL:
+    case GL_STENCIL_BITS:
+    case GL_STENCIL_CLEAR_VALUE:
+    case GL_STENCIL_FAIL:
+    case GL_STENCIL_FUNC:
+    case GL_STENCIL_PASS_DEPTH_FAIL:
+    case GL_STENCIL_PASS_DEPTH_PASS:
+    case GL_STENCIL_REF:
+    case GL_STENCIL_TEST:
+    case GL_STENCIL_VALUE_MASK:
+    case GL_STENCIL_WRITEMASK:
+    case GL_STEREO:
+    case GL_SUBPIXEL_BITS:
+    case GL_TEXTURE_1D:
+    case GL_TEXTURE_BINDING_1D:
+    case GL_TEXTURE_2D:
+    case GL_TEXTURE_BINDING_2D:
+    case GL_TEXTURE_GEN_Q:
+    case GL_TEXTURE_GEN_R:
+    case GL_TEXTURE_GEN_S:
+    case GL_TEXTURE_GEN_T:
+    case GL_TEXTURE_STACK_DEPTH:
+    case GL_UNPACK_ALIGNMENT:
+    case GL_UNPACK_LSB_FIRST:
+    case GL_UNPACK_ROW_LENGTH:
+    case GL_UNPACK_SKIP_PIXELS:
+    case GL_UNPACK_SKIP_ROWS:
+    case GL_UNPACK_SWAP_BYTES:
+    case GL_ZOOM_X:
+    case GL_ZOOM_Y:
+      return 1;
+
+    case GL_DEPTH_RANGE:
+    case GL_LINE_WIDTH_RANGE:
+    case GL_MAP1_GRID_DOMAIN:
+    case GL_MAP2_GRID_SEGMENTS:
+    case GL_MAX_VIEWPORT_DIMS:
+    case GL_POLYGON_MODE:
+      return 2;
+
+    case GL_CURRENT_NORMAL:
+      return 3;
+
+    case GL_ACCUM_CLEAR_VALUE:
+    case GL_BLEND_COLOR_EXT:
+    case GL_COLOR_CLEAR_VALUE:
+    case GL_COLOR_WRITEMASK:
+    case GL_CURRENT_COLOR:
+    case GL_CURRENT_RASTER_COLOR:
+    case GL_CURRENT_RASTER_POSITION:
+    case GL_CURRENT_RASTER_TEXTURE_COORDS:
+    case GL_CURRENT_TEXTURE_COORDS:
+    case GL_FOG_COLOR:
+    case GL_LIGHT_MODEL_AMBIENT:
+    case GL_MAP2_GRID_DOMAIN:
+    case GL_SCISSOR_BOX:
+    case GL_VIEWPORT:
+      return 4;
     case GL_MODELVIEW_MATRIX:
     case GL_PROJECTION_MATRIX:
     case GL_TEXTURE_MATRIX:
       return 16;
-    case GL_VIEWPORT:
-      return 4;
-    case GL_MODELVIEW_STACK_DEPTH:
-    case GL_PROJECTION_STACK_DEPTH:
-    case GL_TEXTURE_STACK_DEPTH:
-      return 1;
+
     default:
-      return 0;
+      {
+	static GLint max_lights = 0;
+	static GLint max_clip_planes = 0;
+	if (!max_lights)
+	  {
+	    glGetIntegerv (GL_MAX_LIGHTS, &max_lights);
+	    glGetIntegerv (GL_MAX_CLIP_PLANES, &max_clip_planes);
+	  }
+
+	/* GL_LIGHTi = 1 */
+	if (pname > GL_LIGHT0 && pname <= GL_LIGHT0 + max_lights)
+	  return 1;
+
+	/* GL_CLIP_PLANEi = 1 */
+	if (pname > GL_CLIP_PLANE0 && pname <= GL_CLIP_PLANE0 + max_clip_planes)
+	  return 1;
+
+	return 0;
+      }
     }
 }
+
 
 static OOP
 gst_opengl_glGetDoublev ( GLenum pname )
@@ -616,6 +1052,7 @@ void gst_initModule_gl()
   vm_proxy->defineCFunc ("glFogfv", glFogfv);
   vm_proxy->defineCFunc ("glFogi", glFogi);
   vm_proxy->defineCFunc ("glFogiv", glFogiv);
+  vm_proxy->defineCFunc ("glFogv", gst_opengl_glFogv);
   vm_proxy->defineCFunc ("glFrontFace", glFrontFace);
   vm_proxy->defineCFunc ("glFrustum", glFrustum);
   vm_proxy->defineCFunc ("glGenLists", glGenLists);
@@ -669,6 +1106,7 @@ void gst_initModule_gl()
   vm_proxy->defineCFunc ("glLightModelfv", glLightModelfv);
   vm_proxy->defineCFunc ("glLightModeli", glLightModeli);
   vm_proxy->defineCFunc ("glLightModeliv", glLightModeliv);
+  vm_proxy->defineCFunc ("glLightModelv", gst_opengl_glLightModelv);
   vm_proxy->defineCFunc ("glLightf", glLightf);
   vm_proxy->defineCFunc ("glLightfv", glLightfv);
   vm_proxy->defineCFunc ("glLighti", glLighti);
@@ -861,11 +1299,11 @@ void gst_initModule_gl()
   vm_proxy->defineCFunc ("glTexGeniv", glTexGeniv);
   vm_proxy->defineCFunc ("glTexImage1D", glTexImage1D);
   vm_proxy->defineCFunc ("glTexImage2D", glTexImage2D);
-  vm_proxy->defineCFunc ("glTexImage2D", glTexImage2D);
   vm_proxy->defineCFunc ("glTexParameterf", glTexParameterf);
   vm_proxy->defineCFunc ("glTexParameterfv", glTexParameterfv);
   vm_proxy->defineCFunc ("glTexParameteri", glTexParameteri);
   vm_proxy->defineCFunc ("glTexParameteriv", glTexParameteriv);
+  vm_proxy->defineCFunc ("glTexParameterv", gst_opengl_glTexParameterv);
   vm_proxy->defineCFunc ("glTranslatev", gst_opengl_glTranslatev);
   vm_proxy->defineCFunc ("glTranslated", glTranslated);
   vm_proxy->defineCFunc ("glTranslatef", glTranslatef);
