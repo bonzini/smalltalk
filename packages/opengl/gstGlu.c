ab32fb5b2b7b3a360f4d803ac80f565679d3e50d
Paolo Bonzini <bonzini@gnu.org>
2008-05-20 08:29:43 +0200

    add license blurb

--- packages/opengl/gstGlu.c
+++ packages/opengl/gstGlu.c
@@ -1,12 +1,78 @@
-/* 
- this file is distributed under the same terms as GNU Smalltalk
-*/
+/******************************** -*- C -*- ****************************
+ *
+ *	OpenGL GLU bindings.
+ *
+ *
+ ***********************************************************************/
+
+/***********************************************************************
+ *
+ * Copyright 2008 Free Software Foundation, Inc.
+ * Written by Paolo Bonzini and Olivier Blanc.
+ *
+ * This file is part of GNU Smalltalk.
+ *
+ * GNU Smalltalk is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2, or (at your option) any later
+ * version.
+ *
+ * Linking GNU Smalltalk statically or dynamically with other modules is
+ * making a combined work based on GNU Smalltalk.  Thus, the terms and
+ * conditions of the GNU General Public License cover the whole
+ * combination.
+ *
+ * In addition, as a special exception, the Free Software Foundation
+ * give you permission to combine GNU Smalltalk with free software
+ * programs or libraries that are released under the GNU LGPL and with
+ * independent programs running under the GNU Smalltalk virtual machine.
+ *
+ * You may copy and distribute such a system following the terms of the
+ * GNU GPL for GNU Smalltalk and the licenses of the other code
+ * concerned, provided that you include the source code of that other
+ * code when and as the GNU GPL requires distribution of source code.
+ *
+ * Note that people who make modified versions of GNU Smalltalk are not
+ * obligated to grant this special exception for their modified
+ * versions; it is their choice whether to do so.  The GNU General
+ * Public License gives permission to release a modified version without
+ * this exception; this exception also makes it possible to release a
+ * modified version which carries forward this exception.
+ *
+ * GNU Smalltalk is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * GNU Smalltalk; see the file COPYING.	 If not, write to the Free Software
+ * Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ ***********************************************************************/
+
 #include "gstopengl.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define nil  vm_proxy->nilOOP
+
+void gst_opengl_gluLookAtv (OOP eyeOOP, OOP centerOOP, OOP upOOP)
+{
+  GLfloat eye[3], *peye ;
+  GLfloat center[3], *pcenter ;
+  GLfloat up[3], *pup ;
+
+  peye = gst_opengl_oop_to_array (eye, peye, 3);
+  pcenter = gst_opengl_oop_to_array (center, pcenter, 3);
+  pup = gst_opengl_oop_to_array (up, pup, 3);
+
+  if (!peye || !pcenter || !pup)
+    return;					/* Should fire an exception */
+
+  gluLookAt (peye[0], peye[1], peye[2], pcenter[0], pcenter[1], pcenter[2],
+	      pup[0], pup[1], pup[2]) ;
+}
 
 void gst_opengl_gluLoadSamplingMatrices (OOP nurb, OOP modelMatrix, OOP projectionMatrix, OOP viewportVertex)
 {
@@ -207,6 +273,7 @@ void gst_initModule_glu() {
   vm_proxy->defineCFunc ("gluGetTessProperty", gluGetTessProperty) ;
   vm_proxy->defineCFunc ("gluLoadSamplingMatrices", gst_opengl_gluLoadSamplingMatrices) ;
   vm_proxy->defineCFunc ("gluLookAt", gluLookAt) ;
+  vm_proxy->defineCFunc ("gluLookAtv", gst_opengl_gluLookAtv) ;
   vm_proxy->defineCFunc ("gluNextContour", gluNextContour) ;
   vm_proxy->defineCFunc ("gluOrtho2D", gluOrtho2D) ;
   vm_proxy->defineCFunc ("gluPartialDisk", gst_opengl_gluPartialDisk) ;
