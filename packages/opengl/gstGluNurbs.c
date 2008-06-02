67e78391a2f55bf7959201d8b1de3bdd8bce5f9e
Olivier Blanc <olivier.blanc@laposte.net>
2008-06-02 17:18:30 +0200

    redo nurbs callbacks to attach callback functions to nurbs objects
    packages/opengl:
    2008-06-02  Olivier Blanc  <olivier.blanc@laposte.net>
    
    	* OpenGLUNurbs.st: Change the callback implementation to attach
    	callback functions to nurbs objects.
    	* gstGluNurbs.c: Likewise.
    	* test/surfpoints.st: Likewise.

--- packages/opengl/gstGluNurbs.c
+++ packages/opengl/gstGluNurbs.c
@@ -58,236 +58,117 @@
 
 #define nil  vm_proxy->nilOOP
 
-/* Number of known callbacks */
-#define N_GLU_CALLBACKS 13
-
-/* Receivers for the callbacks; initialized in module init func */
-static OOP gluNurbReceiver[N_GLU_CALLBACKS] ;
-
-/* Selectors; initialized in module init func */
-static OOP gluNurbSelector[N_GLU_CALLBACKS] ;
-
-/* Callback constants definition */
-static GLenum gluNurbFunctions[N_GLU_CALLBACKS] = {
-  GLU_NURBS_ERROR,
-  GLU_NURBS_BEGIN,
-  GLU_NURBS_VERTEX,
-  GLU_NURBS_NORMAL,
-  GLU_NURBS_COLOR,
-  GLU_NURBS_TEXTURE_COORD,
-  GLU_NURBS_END,
-  GLU_NURBS_BEGIN_DATA,
-  GLU_NURBS_VERTEX_DATA,
-  GLU_NURBS_NORMAL_DATA,
-  GLU_NURBS_COLOR_DATA,
-  GLU_NURBS_TEXTURE_COORD_DATA,
-  GLU_NURBS_END_DATA
-} ;
-
-
-/* Retreive the callback index from the Callback contant definition */
-int gst_glu_nurbCallback_getCallbackIndex(GLenum which)
-{
-  int i ;
-  for (i = 0 ; i < N_GLU_CALLBACKS ; ++i) {
-	/* If function is found, return index */
-	if (gluNurbFunctions[i] == which) {
-	  return i ;
-	}
-  }
-  return -1 ;
-}
-
 /* Send the callback message to the receiver */
-void gst_glu_nurbCallback_sendMessage(GLenum which, OOP* args, int nArgs)
+void gst_glu_nurbCallback_sendMessageTo (OOP receiver, GLenum which,
+					 OOP* args, int nArgs)
 {
-  /* Get the callback index */
-  int index = gst_glu_nurbCallback_getCallbackIndex(which) ;
+  OOP aBlock;			/* Callback's block */
 
-  if(index == -1) return ;
-  /* verify the receiver and the selector */
-  if(gluNurbReceiver[index] != nil && 
-	 gluNurbSelector[index] != nil) 
-	{
-	  /* Send the callback message */
-	  if(nArgs > 0) 
-		vm_proxy->nvmsgSend(gluNurbReceiver[index], 
-									gluNurbSelector[index],
-									args, nArgs) ;
-	  else
-		vm_proxy->perform(gluNurbReceiver[index], 
-								  gluNurbSelector[index]) ;
-	}
-}
-/* Define the C Callback functions and call the sendMessage function */
-void gst_glu_nurbCallback_Nurbs_Error (GLenum errorCode)
-{
-  OOP* args = alloca ( sizeof(OOP) * 1) ;
-  args[0] = vm_proxy->intToOOP(errorCode) ;
-  gst_glu_nurbCallback_sendMessage(GLU_NURBS_ERROR, args, 1) ;
-}
-
-void gst_glu_nurbCallback_Nurbs_Begin ( GLenum type )
-{
-  OOP* args = alloca ( sizeof(OOP) * 1) ;
-  args[0] = vm_proxy->intToOOP(type) ;
-  gst_glu_nurbCallback_sendMessage(GLU_NURBS_BEGIN, args, 1) ;
-}
-
-void gst_glu_nurbCallback_Nurbs_Vertex ( GLfloat* vertex )
-{
-  OOP* args = alloca ( sizeof(OOP) * 3 ) ;
-  args[0] = vm_proxy->floatToOOP( vertex[0] ) ;
-  args[1] = vm_proxy->floatToOOP( vertex[1] ) ;
-  args[2] = vm_proxy->floatToOOP( vertex[2] ) ;
-  gst_glu_nurbCallback_sendMessage(GLU_NURBS_VERTEX, args, 3) ;
-}
-
-void gst_glu_nurbCallback_Nurbs_Normal ( GLfloat* normal )
-{
-  OOP* args = alloca ( sizeof(OOP) * 3 ) ;
-  args[0] = vm_proxy->floatToOOP( normal[0] ) ;
-  args[1] = vm_proxy->floatToOOP( normal[1] ) ;
-  args[2] = vm_proxy->floatToOOP( normal[2] ) ;
-  gst_glu_nurbCallback_sendMessage(GLU_NURBS_NORMAL, args, 3) ;
-}
-
-void gst_glu_nurbCallback_Nurbs_Color ( GLfloat* color)
-{
-  OOP* args = alloca ( sizeof(OOP) * 3 ) ;
-  args[0] = vm_proxy->floatToOOP( color[0] ) ;
-  args[1] = vm_proxy->floatToOOP( color[1] ) ;
-  args[2] = vm_proxy->floatToOOP( color[2] ) ;
-  gst_glu_nurbCallback_sendMessage(GLU_NURBS_COLOR, args, 3) ;
-}
-
-void gst_glu_nurbCallback_Nurbs_Texture_Coord ( GLfloat* tCrd )
-{
-  OOP* args = alloca ( sizeof(OOP) * 3 ) ;
-  args[0] = vm_proxy->floatToOOP( tCrd[0] ) ;
-  args[1] = vm_proxy->floatToOOP( tCrd[1] ) ;
-  args[2] = vm_proxy->floatToOOP( tCrd[2] ) ;
-  gst_glu_nurbCallback_sendMessage(GLU_NURBS_TEXTURE_COORD, args, 3) ;
-}
-
-void gst_glu_nurbCallback_Nurbs_End ( )
-{
-  gst_glu_nurbCallback_sendMessage(GLU_NURBS_END, &nil, 0) ;
+  /* Retreive the callback block */
+  vm_proxy->msgSendf (&aBlock, "%o %o getCallback: %i", receiver, which);
+  if (aBlock != nil)
+    vm_proxy->nvmsgSend(aBlock, NULL, args, nArgs);
 }
 
 void gst_glu_nurbCallback_Nurbs_Begin_Data ( GLenum type, void* data )
 {
-  OOP* args = alloca ( sizeof(OOP) * 2) ;
-  args[0] = vm_proxy->intToOOP(type) ;
-  args[1] = (OOP) data ;
-  gst_glu_nurbCallback_sendMessage(GLU_NURBS_BEGIN_DATA, args, 2) ;
+  OOP args[1];
+  args[0] = vm_proxy->intToOOP(type);
+  gst_glu_nurbCallback_sendMessageTo((OOP)data, GLU_NURBS_BEGIN, args, 1);
 }
 
 void gst_glu_nurbCallback_Nurbs_Vertex_Data ( GLfloat* vertex, void* data )
 {
-  OOP* args = alloca ( sizeof(OOP) * 4 ) ;
-  args[0] = vm_proxy->floatToOOP( vertex[0] ) ;
-  args[1] = vm_proxy->floatToOOP( vertex[1] ) ;
-  args[2] = vm_proxy->floatToOOP( vertex[2] ) ;
-  args[3] = (OOP) data ;
-  gst_glu_nurbCallback_sendMessage(GLU_NURBS_VERTEX_DATA, args, 4) ;
+  OOP args[3];
+  args[0] = vm_proxy->floatToOOP( vertex[0] );
+  args[1] = vm_proxy->floatToOOP( vertex[1] );
+  args[2] = vm_proxy->floatToOOP( vertex[2] );
+  gst_glu_nurbCallback_sendMessageTo((OOP)data, GLU_NURBS_VERTEX, args, 3);
 }
 
 void gst_glu_nurbCallback_Nurbs_Normal_Data ( GLfloat* normal, void* data )
 {
-  OOP* args = alloca ( sizeof(OOP) * 4 ) ;
-  args[0] = vm_proxy->floatToOOP( normal[0] ) ;
-  args[1] = vm_proxy->floatToOOP( normal[1] ) ;
-  args[2] = vm_proxy->floatToOOP( normal[2] ) ;
-  args[3] = (OOP) data ;
-  gst_glu_nurbCallback_sendMessage(GLU_NURBS_NORMAL_DATA, args, 4) ;
+  OOP args[3];
+  args[0] = vm_proxy->floatToOOP( normal[0] );
+  args[1] = vm_proxy->floatToOOP( normal[1] );
+  args[2] = vm_proxy->floatToOOP( normal[2] );
+  gst_glu_nurbCallback_sendMessageTo((OOP)data, GLU_NURBS_NORMAL, args, 3);
 }
 
 void gst_glu_nurbCallback_Nurbs_Color_Data ( GLfloat* color, void* data)
 {
-  OOP* args = alloca ( sizeof(OOP) * 4 ) ;
-  args[0] = vm_proxy->floatToOOP( color[0] ) ;
-  args[1] = vm_proxy->floatToOOP( color[1] ) ;
-  args[2] = vm_proxy->floatToOOP( color[2] ) ;
-  args[3] = (OOP) data ;
-  gst_glu_nurbCallback_sendMessage(GLU_NURBS_COLOR_DATA, args, 4) ;
+  OOP args[3];
+  args[0] = vm_proxy->floatToOOP( color[0] );
+  args[1] = vm_proxy->floatToOOP( color[1] );
+  args[2] = vm_proxy->floatToOOP( color[2] );
+  gst_glu_nurbCallback_sendMessageTo((OOP)data, GLU_NURBS_COLOR, args, 3);
 }
 
 void gst_glu_nurbCallback_Nurbs_Texture_Coord_Data ( GLfloat* tCrd, void* data )
 {
-  OOP* args = alloca ( sizeof(OOP) * 4 ) ;
-  args[0] = vm_proxy->floatToOOP( tCrd[0] ) ;
-  args[1] = vm_proxy->floatToOOP( tCrd[1] ) ;
-  args[2] = vm_proxy->floatToOOP( tCrd[2] ) ;
-  args[3] = (OOP) data ;
-  gst_glu_nurbCallback_sendMessage(GLU_NURBS_TEXTURE_COORD_DATA, args, 4) ;
+  OOP args[3];
+  args[0] = vm_proxy->floatToOOP( tCrd[0] );
+  args[1] = vm_proxy->floatToOOP( tCrd[1] );
+  args[2] = vm_proxy->floatToOOP( tCrd[2] );
+  gst_glu_nurbCallback_sendMessageTo((OOP)data, GLU_NURBS_TEXTURE_COORD, args, 3);
 }
 
 void gst_glu_nurbCallback_Nurbs_End_Data (void* data )
 {
-  OOP* args = alloca ( sizeof(OOP) * 1 ) ;
-  args[0] = (OOP) data ;
-  gst_glu_nurbCallback_sendMessage(GLU_NURBS_END_DATA, args, 1) ;
+  gst_glu_nurbCallback_sendMessageTo((OOP)data, GLU_NURBS_END, NULL, 0);
+}
+
+/* Pointers to the Glu Nurbs callback functions */
+struct gst_glu_callback {
+  GLenum which;
+  GLenum whichData;
+  GLUfuncptr func;
+};
+
+/* Number of known callbacks */
+#define N_GLU_CALLBACKS 6
+
+static struct gst_glu_callback gst_glu_nurbCallbackFuncs[] = {
+  { GLU_NURBS_BEGIN, GLU_NURBS_BEGIN_DATA,
+    (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Begin_Data },
+  { GLU_NURBS_VERTEX, GLU_NURBS_VERTEX_DATA,
+    (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Vertex_Data },
+  { GLU_NURBS_NORMAL, GLU_NURBS_NORMAL_DATA,
+    (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Normal_Data },
+  { GLU_NURBS_COLOR, GLU_NURBS_COLOR_DATA,
+    (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Color_Data },
+  { GLU_NURBS_TEXTURE_COORD, GLU_NURBS_TEXTURE_COORD_DATA,
+    (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Texture_Coord_Data },
+  { GLU_NURBS_END, GLU_NURBS_END_DATA,
+    (GLUfuncptr) gst_glu_nurbCallback_Nurbs_End_Data }
+};
+
+/* Retreive the callback index from the Callback contant definition */
+struct gst_glu_callback *gst_glu_nurb_getCallbackIndex(GLenum which)
+{
+  int i;
+  for (i = 0; i < N_GLU_CALLBACKS; ++i)
+	/* If function is found, return index */
+	if (gst_glu_nurbCallbackFuncs[i].which == which)
+	  return &gst_glu_nurbCallbackFuncs[i];
+
+  return (struct gst_glu_callback *) NULL;
 }
 
 
-/* Pointers to the Glu Nurbs callback functions */
-static GLUfuncptr gst_glu_nurbCallback[N_GLU_CALLBACKS] = {
-  (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Error,
-  (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Begin,
-  (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Vertex,
-  (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Normal,
-  (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Color,
-  (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Texture_Coord,
-  (GLUfuncptr) gst_glu_nurbCallback_Nurbs_End,
-  (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Begin_Data,
-  (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Vertex_Data,
-  (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Normal_Data,
-  (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Color_Data,
-  (GLUfuncptr) gst_glu_nurbCallback_Nurbs_Texture_Coord_Data,
-  (GLUfuncptr) gst_glu_nurbCallback_Nurbs_End_Data
-} ;
+void
+gst_opengl_gluNurbsConnectSignal (OOP nurbOOP, GLenum which)
+{
+  struct gst_glu_callback *cb_data = gst_glu_nurb_getCallbackIndex (which);
+  GLUnurbs *nurb = vm_proxy->OOPToCObject (nurbOOP);
 
+  if (!cb_data)
+    return;			/* Should fire an interrupt */
 
-
-void gst_opengl_gluNurbsCallback (OOP nurb, GLenum which, OOP receiver, OOP selector)
-{ 
-  int index = gst_glu_nurbCallback_getCallbackIndex(which) ;
-
-  if(index == -1) 
-	return ;
-
-  /* Unregister previous receiver and selector callback */
-  if(gluNurbReceiver[index] != nil) 
-	{
-	  vm_proxy->unregisterOOP(gluNurbReceiver[index]) ;
-	  gluNurbReceiver[index] = nil ;
-	}
-  if(gluNurbSelector[index] != nil) 
-	{
-	  vm_proxy->unregisterOOP(gluNurbSelector[index]) ;
-	  gluNurbSelector[index] = nil ;
-	}
-  /* Verify if selector and receiver are not nil. Else clear the callback */
-  if(receiver != nil && selector != nil ) 
-	{
-	  /* Assign new receiver and selector and register them*/
-	  gluNurbReceiver[index] = receiver ;
-	  gluNurbSelector[index] = selector ;
-	  vm_proxy->registerOOP(gluNurbReceiver[index]) ;
-	  vm_proxy->registerOOP(gluNurbSelector[index]) ;
-
-	  /* Setting up the callback pointer */
-	  gluNurbsCallback (vm_proxy->OOPToCObject(nurb), which, gst_glu_nurbCallback[index]) ;
-	}
-  else 
-	{
-	  gluNurbReceiver[index] = nil ;
-	  gluNurbSelector[index] = nil ;
-
-	  /* Cleaning up the callback */
-	  gluNurbsCallback (vm_proxy->OOPToCObject(nurb), which, NULL) ;
-	}
+  /* Add a pointer to the nurb object to retreive informations during
+     the callback */
+  gluNurbsCallbackData (nurb, (void *) nurbOOP);
+  gluNurbsCallback (nurb, cb_data->whichData, cb_data->func);
 }
 
 int gst_opengl_gluNurbsCurve (GLUnurbs *nurb, 
@@ -295,28 +176,28 @@ int gst_opengl_gluNurbsCurve (GLUnurbs *nurb,
 				OOP control,
 				GLint order, GLenum type)
 {
-  GLfloat* knotsFloat ;
-  GLfloat* controlFloat ;
-  size_t nObjs, size ;
+  GLfloat* knotsFloat;
+  GLfloat* controlFloat;
+  size_t nObjs, size;
 
   size = gst_opengl_glMap_size (type);
   if (!size)
     return GL_INVALID_ENUM;
 
   /* Retreive knots and put them in an array */
-  knotsFloat = alloca (sizeof(GLfloat) * knotCount) ;
-  knotsFloat = gst_opengl_oop_to_array (knotsFloat, knots, knotCount) ;
+  knotsFloat = alloca (sizeof(GLfloat) * knotCount);
+  knotsFloat = gst_opengl_oop_to_array (knotsFloat, knots, knotCount);
   if(!knotsFloat) 
 	return GL_INVALID_VALUE;		/* Should fire an interrupt */
 
   /* Retreive control points and put them in an array */
   nObjs = (order - 1) * stride + size;
-  controlFloat = alloca (sizeof(GLfloat) * nObjs) ;
-  controlFloat = gst_opengl_oop_to_array (controlFloat, control, nObjs ) ;
+  controlFloat = alloca (sizeof(GLfloat) * nObjs);
+  controlFloat = gst_opengl_oop_to_array (controlFloat, control, nObjs );
   if(!controlFloat)
 	return GL_INVALID_VALUE;		/* Should fire an interrupt */
   
-  gluNurbsCurve (nurb, knotCount, knotsFloat, stride, controlFloat, order, type) ;
+  gluNurbsCurve (nurb, knotCount, knotsFloat, stride, controlFloat, order, type);
 
   return 0;
 }
@@ -328,59 +209,53 @@ int gst_opengl_gluNurbsSurface (GLUnurbs *nurb,
 				OOP control,
 				GLint sOrder, GLint tOrder, GLenum type)
 {
-  GLfloat* sKnotsFloat ;
-  GLfloat* tKnotsFloat ;
-  GLfloat* controlFloat ;
-  size_t nObjs, size ;
+  GLfloat* sKnotsFloat;
+  GLfloat* tKnotsFloat;
+  GLfloat* controlFloat;
+  size_t nObjs, size;
 
   size = gst_opengl_glMap_size (type);
   if (!size)
     return GL_INVALID_ENUM;
 
   /* Retreive sKnots and put them in an array */
-  sKnotsFloat = alloca (sizeof(GLfloat) * sKnotCount) ;
-  sKnotsFloat = gst_opengl_oop_to_array (sKnotsFloat, sKnots, sKnotCount) ;
+  sKnotsFloat = alloca (sizeof(GLfloat) * sKnotCount);
+  sKnotsFloat = gst_opengl_oop_to_array (sKnotsFloat, sKnots, sKnotCount);
   if(!sKnotsFloat) 
 	return GL_INVALID_VALUE;		/* Should fire an interrupt */
 
   /* Retreive tKnots and put them in an array */
-  tKnotsFloat = alloca (sizeof(GLfloat) * tKnotCount) ;
-  tKnotsFloat = gst_opengl_oop_to_array (tKnotsFloat, tKnots, sKnotCount) ;
+  tKnotsFloat = alloca (sizeof(GLfloat) * tKnotCount);
+  tKnotsFloat = gst_opengl_oop_to_array (tKnotsFloat, tKnots, sKnotCount);
   if(!tKnotsFloat) 
 	return GL_INVALID_VALUE;		/* Should fire an interrupt */
   
   /* Retreive control points and put them in an array */
   nObjs = (sOrder - 1) * sStride + (tOrder - 1) * tStride + size;
-  controlFloat = alloca (sizeof(GLfloat) * nObjs) ;
-  controlFloat = gst_opengl_oop_to_array (controlFloat, control, nObjs ) ;
+  controlFloat = alloca (sizeof(GLfloat) * nObjs);
+  controlFloat = gst_opengl_oop_to_array (controlFloat, control, nObjs );
   if(!controlFloat)
 	return GL_INVALID_VALUE;		/* Should fire an interrupt */
   
   gluNurbsSurface (nurb, sKnotCount, sKnotsFloat, tKnotCount,
 		   tKnotsFloat, sStride, tStride,
-		   controlFloat, sOrder, tOrder, type) ;
+		   controlFloat, sOrder, tOrder, type);
 
   return 0;
 }
 
 /* Init module */
 void gst_initModule_gluNurbs() {
-  int i ;
-  vm_proxy = vm_proxy ;
-
-  /* Initialize the Nurbs callback selector and receivers */
-  for (i = 0 ; i <N_GLU_CALLBACKS ; ++i )
-	gluNurbReceiver[i] = gluNurbSelector[i] = nil ;
-
   /* Define C Functions */
-  vm_proxy->defineCFunc ("gluNurbsCallback", gst_opengl_gluNurbsCallback) ;
-  // vm_proxy->defineCFunc ("gluNurbsCallbackData", gluNurbsCallbackData) ;
-  // vm_proxy->defineCFunc ("gluNurbsCallbackDataEXT", gluNurbsCallbackDataEXT) ;
-  // vm_proxy->defineCFunc ("gluGetNurbsProperty", gluGetNurbsProperty) ;
-  vm_proxy->defineCFunc ("gluNewNurbsRenderer", gluNewNurbsRenderer) ;
-  vm_proxy->defineCFunc ("gluDeleteNurbsRenderer", gluDeleteNurbsRenderer) ;
-  vm_proxy->defineCFunc ("gluNurbsProperty", gluNurbsProperty) ;
-  vm_proxy->defineCFunc ("gluNurbsCurve", gst_opengl_gluNurbsCurve) ;
-  vm_proxy->defineCFunc ("gluNurbsSurface", gst_opengl_gluNurbsSurface) ;
+  // vm_proxy->defineCFunc ("gluNurbsCallback", gst_opengl_gluNurbsCallback);
+  // vm_proxy->defineCFunc ("gluNurbsCallbackData", gluNurbsCallbackData);
+  // vm_proxy->defineCFunc ("gluNurbsCallbackDataEXT", gluNurbsCallbackDataEXT);
+  vm_proxy->defineCFunc ("gluGetNurbsProperty", gluGetNurbsProperty);
+  vm_proxy->defineCFunc ("gluNewNurbsRenderer", gluNewNurbsRenderer);
+  vm_proxy->defineCFunc ("gluDeleteNurbsRenderer", gluDeleteNurbsRenderer);
+  vm_proxy->defineCFunc ("gluNurbsProperty", gluNurbsProperty);
+  vm_proxy->defineCFunc ("gluNurbsCurve", gst_opengl_gluNurbsCurve);
+  vm_proxy->defineCFunc ("gluNurbsSurface", gst_opengl_gluNurbsSurface);
 
+  vm_proxy->defineCFunc ("gluNurbsConnectSignal", gst_opengl_gluNurbsConnectSignal);
 }
